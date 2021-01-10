38172c9928e03e4876b3528995d00f78203af229
Natalya Anatskaya <taff2011@yandex.ru>
2021-01-11 01:26:45 +0500

    Проектная работа №4

--- sctipts/index.js
+++ sctipts/index.js
@@ -1,38 +1,43 @@
 let openButton = document.querySelector('.profile__icon')
-let overlay = document.querySelector('.overlay')
-let closeButton = overlay.querySelector('.popup__close-icon')
+let popup = document.querySelector('.popup')
+let closeButton = popup.querySelector('.popup__close-icon')
 
 
-/*openButton.addEventListener('click',() => {
-    overlay.classList.toggle('overlay_active')
-})
-
-closeButton.addEventListener('click',() => {
-      overlay.classList.toggle('overlay_active')
-})*/
-function toddlePopup (){
-    overlay.classList.toggle('overlay_active')
+function popupOpen() {
+   
+    popup.classList.add('popup_active')  
 }
-openButton.addEventListener('click', toddlePopup)
-closeButton.addEventListener('click', toddlePopup)
+openButton.addEventListener('click', popupOpen)
 
-let formElement = document.querySelector('.popup')
+function popupClose() {
 
+    popup.classList.remove('popup_active')  
+}
+closeButton.addEventListener('click', popupClose)
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
+let formElement = document.querySelector('.popup__form')
 
-    let nameInput = formElement.querySelector('.popup__input-name')// Воспользуйтесь инструментом .querySelector()
-    let jobInput = formElement.querySelector('.popup__input-info')// Воспользуйтесь инструментом .querySelector()
- 
-      // Получите значение полей из свойства value
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
+let nameInput = formElement.querySelector('.popup__info_input_name')
+let jobInput = formElement.querySelector('.popup__info_input_job')
 
 let nameProfile = document.querySelector('.profile__name')
-let jobProfile = document.querySelector('.profile__info')
+let jobProfile = document.querySelector('.profile__job')
+
+let saveButtom = document.querySelector('.popup__button')
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+
+  let inputs = document.querySelectorAll('input')
+    console.log(inputs[0].value); 
+    console.log(inputs[1].value); 
+ 
+
 
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
+
 }
 formElement.addEventListener('submit', handleFormSubmit);
+saveButtom.addEventListener('click', popupClose);
