6826700134cf30eb4057900a0d0bb61b17c113ec
Natalya Anatskaya <taff2011@yandex.ru>
2021-01-12 20:12:18 +0500

    Проектная работа №4

--- sctipts/index.js
+++ sctipts/index.js
@@ -1,12 +1,18 @@
 let openButton = document.querySelector('.profile__icon')
 let popup = document.querySelector('.popup')
 let closeButton = document.querySelector('.popup__close-icon')
-
+let formElement = document.querySelector('.popup__form')
+let nameInput = formElement.querySelector('.popup__info_input_name')
+let jobInput = formElement.querySelector('.popup__info_input_job')
+let nameProfile = document.querySelector('.profile__name')
+let jobProfile = document.querySelector('.profile__job')
 
 function popupOpen() {
    
     popup.classList.add('popup_active')
-    nameProfile.textContent = nameInput.value;  
+
+    nameInput.value = nameProfile.innerText
+    jobInput.value = jobProfile.innerText
 }
 openButton.addEventListener('click', popupOpen)
 
@@ -15,24 +21,6 @@ function popupClose() {
     popup.classList.remove('popup_active')  
 }
 closeButton.addEventListener('click', popupClose)
-
-let formElement = document.querySelector('.popup__form')
-
-let nameInput = formElement.querySelector('.popup__info_input_name')
-let jobInput = formElement.querySelector('.popup__info_input_job')
-
-let nameProfile = document.querySelector('.profile__name')
-let jobProfile = document.querySelector('.profile__job')
-
-
-function sendValueInInput(){
-
-    nameInput.value = nameProfile.innerText
-    jobInput.value = jobProfile.innerText
-
-}
-sendValueInInput()
-
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -43,4 +31,5 @@ function handleFormSubmit (evt) {
     popupClose()
 }
 formElement.addEventListener('submit', handleFormSubmit);
+/* что же я такая непонятливая-то(((*/
 
