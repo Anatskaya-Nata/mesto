21fd6a176e0e371c10286a638b00842021e5c1cf
Natalya Anatskaya <taff2011@yandex.ru>
2021-01-23 21:07:09 +0500

    основные карточки в JS

---
+++ sctipts/addingcards.js
@@ -0,0 +1,37 @@
+const gallaryCards = document.querySelector('.gallary__cards');
+const gallaryTemplate = document.querySelector('.gallary__template').content;
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+  initialCards.forEach(function (element) {
+    const galleryVew = gallaryTemplate.cloneNode(true);
+
+    galleryVew.querySelector(".gallary__text").textContent = element.name
+    galleryVew.querySelector(".gallary__item").src = element.link
+    gallaryCards.append(galleryVew)
+  })
